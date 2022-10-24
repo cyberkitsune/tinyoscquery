31d27e3597a1ef50b905b50ee009803a6d8712ff
CyberKitsune <cyberkitsune09@gmail.com>
2022-10-23 19:08:36 -0700

    utility: Add docstring

--- tinyoscquery/utility.py
+++ tinyoscquery/utility.py
@@ -1,6 +1,12 @@
 import socket
 
 def get_open_tcp_port():
+    '''
+    Returns a valid, open, TCP port.
+
+        Returns:
+            port (int): A TCP port that is able to be bound to
+    '''
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("", 0))
     port = s.getsockname()[1]
@@ -8,6 +14,12 @@ def get_open_tcp_port():
     return port
 
 def get_open_udp_port():
+    '''
+    Returns a valid, open, UDP port.
+
+        Returns:
+            port (int): A UDP port that is able to be bound to
+    '''
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(("", 0))
     port = s.getsockname()[1]
