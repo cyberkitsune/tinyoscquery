417a663b5c1a1442e234f46e3931d4218728ecb6
CyberKitsune <cyberkitsune09@gmail.com>
2024-10-05 16:54:51 -0700

    Setup: Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tinyoscquery",
-    version='0.1.2',
+    version='0.1.3',
     description="Quick and dirty python implementation for OSCQuery",
     author="CyberKitsune",
     packages=['tinyoscquery', 'tinyoscquery.shared'],
